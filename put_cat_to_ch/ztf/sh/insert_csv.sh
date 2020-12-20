c7caf8a4e3d8c71461f8482ef2f7c29c974d7399
Konstantin Malanchev <hombit@gmail.com>
2020-12-20 19:19:40 +0300

    Fix curl command

--- put_cat_to_ch/ztf/sh/insert_csv.sh
+++ put_cat_to_ch/ztf/sh/insert_csv.sh
@@ -4,5 +4,7 @@ FILE=$1
 TABLE=$2
 HOST=$3
 
-curl "http://${HOST}:8123/?query=INSERT%20INTO%20${TABLE}%20FORMAT%20CSV" \
+curl \
+    "http://${HOST}:8123/?query=INSERT%20INTO%20${TABLE}%20FORMAT%20CSV" \
+    --data-binary @- \
   < ${FILE}
