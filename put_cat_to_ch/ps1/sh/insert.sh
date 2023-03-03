89a638edf76f3c1217080512b5c7e0dac5615515
Konstantin Malanchev <hombit@gmail.com>
2023-03-03 10:21:46 -0600

    Fix ps1/sh/insert.sh

--- put_cat_to_ch/ps1/sh/insert.sh
+++ put_cat_to_ch/ps1/sh/insert.sh
@@ -8,5 +8,5 @@ clickhouse-client \
     --query "INSERT INTO ${TABLE} FORMAT CSV" \
     --input_format_parallel_parsing=0 \
     -h ${HOST} \
-    --http_receive_timeout=86400 --http_send_timeout=86400 --http_connection_timeout=86400
+    --http_receive_timeout=86400 --http_send_timeout=86400 --http_connection_timeout=86400 \
  < ${FILE}
