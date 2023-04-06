920a3943ea96e2e85fc1042ac26647d5cda324a1
Konstantin Malanchev <hombit@gmail.com>
2023-04-06 17:00:45 -0500

    Fix

--- put_cat_to_ch/ps1_strm/sql/create_table.sql
+++ put_cat_to_ch/ps1_strm/sql/create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE {if_not_exists} {db}.{table}
 (
     {columns},
-    h3index10 UInt64 MATERIALIZED geoToH3(ra, dec, 10)
+    h3index10 UInt64 MATERIALIZED geoToH3(raMean, decMean, 10)
 )
 ENGINE = MergeTree
-ORDER BY h3index10
+ORDER BY objID
