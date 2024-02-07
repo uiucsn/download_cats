9ebf6e81a7f6e5dc4556d32d4981bb874d21901b
Konstantin Malanchev <hombit@gmail.com>
2024-02-07 12:53:27 -0500

    fix

--- put_cat_to_ch/ztf_metadata/fields.py
+++ put_cat_to_ch/ztf_metadata/fields.py
@@ -5,5 +5,5 @@ def get_rcid_centers():
     geometry = Fields.get_field_geometry(level="quadrant")
     centroid_df = geometry.centroid.get_coordinates()
     # Rename fieldid to field
-    centroid_df.index = centroid_df.index.set_name("field", level=0)
+    centroid_df.index = centroid_df.index.set_names("field", level=0)
     return centroid_df.rename(columns={"x": "ra", "y": "dec"})
